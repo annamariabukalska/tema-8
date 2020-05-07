214b2aa0792960fa1597b1f0584098f88113f760
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2020-05-07 11:24:51 +0200

    newsapi

---
+++ svelte-newsapi/src/App.svelte
@@ -0,0 +1,60 @@
+<script>
+	const api_key = 'a5356865ca494805be50c304df2b02f7'
+	let q = ''
+	let article
+	const getNews = () => {
+		fetch(`https://newsapi.org/v2/everything?q=${q}&apiKey=${api_key}`)
+			.then( res => res.json() )
+				.then( json => {
+					console.log(json.articles[0])
+					article = json.articles[0]
+				})
+	}
+</script>
+
+<main>
+<header>
+	<input placeholder="type to search" type="text" bind:value={q}>
+	<button on:click={getNews}>ok</button>
+</header>
+
+{#if article}
+<a href="{article.url}">
+	<div class="article">
+		<h1>{ article.title }</h1>
+		<p>{article.description}</p>
+	</div>
+</a>
+{:else}
+	<h2>Skriv noe og trykk ok..</h2>
+{/if}
+
+</main>
+
+<style>
+	:global(body, html){
+		margin:0;
+		padding:0;
+	}
+	:global(*){
+		box-sizing:border-box;
+	}
+	main{
+		display:grid;
+		place-items:center;
+		height:100%;
+	}
+	header{
+		position:absolute;
+		top:2rem;
+		width:100%;
+		display:grid;
+		padding: 0 20vw 0 20vw;
+	}
+	.article{
+		width:40vw;
+		height:40vh;
+		background-color:#eee;
+		padding:2rem;
+	}
+</style>
