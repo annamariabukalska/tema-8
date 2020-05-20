ed6db61ca6fdb38f809336a3446434cd509c84bd
annamariabukalska <56019427+annamariabukalska@users.noreply.github.com>
2020-05-20 13:53:35 +0200

    Added css

--- parsing-json-plants/index.js
+++ parsing-json-plants/index.js
@@ -2525,3 +2525,21 @@ const plantliste =
         ]
       }
 
+
+
+let inp = document.querySelector("#search")
+let results = document.querySelector("#plants")
+
+plantliste.plants.map ( plant => 
+    results.innerHTML +=  "<div class='plant'><h2>" + plant.name + "</h2><p>by: " + plant.species + "</p></div>"
+)
+
+inp.addEventListener("input", e => {
+    results.innerHTML = ""
+    const filtered = plantliste.plants.filter( plant => plant.name.includes(e.target.value))
+    filtered.map(plant => 
+        results.innerHTML += "<div class='plant'><h2>" + plant.name + "</h2><p> " + plant.species + "</p></div>"
+        )
+} )
+
+
